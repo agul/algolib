489f53bbf9e3f2a37244c48ecce20eaa0803c28e
Alexander Agulenko <alexander.agulenko@gmail.com>
2016-02-13 11:29:47 +0300

    Minor changes in Head.h

--- Head.h
+++ Head.h
@@ -38,11 +38,13 @@ using namespace std;
 #define LLU "%I64u"
 #define popcount(a) __popcnt(a)
 #define clz(a) __clz(a)
+#define ctz(a) __ctz(a)
 #else
 #define LLD "%lld"
 #define LLU "%llu"
 #define popcount(a) __builtin_popcount(a)
 #define clz(a) __builtin_clz(a)
+#define ctz(a) __builtin_ctz(a)
 #endif
 
 #define fill(a, x) memset(a, x, sizeof(a))
@@ -70,22 +72,31 @@ const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
 const int DX[] = { -1, 0, 1, 0, -1, -1, 1, 1};
 const int DY[] = { 0, -1, 0, 1, -1, 1, 1, -1};
 
+template<class T>
+inline bool is_equal_to_zero(const T& a) {
+	if (std::is_floating_point<T>()) {
+		return abs(a) < EPS;
+	}
+	return a == 0;
+}
+
+
 template<class T> 
-inline void addMod(T& a, const T& b, const T& mod = 1000000007) {
+inline void add_mod(T& a, const T& b, const T& mod = 1000000007) {
 	if ((a += b) >= mod) {
 		a -= mod;
 	}
 }
 
 template<class T> 
-inline void subMod(T& a, const T& b, const T& mod = 1000000007) {
+inline void sub_mod(T& a, const T& b, const T& mod = 1000000007) {
 	if ((a -= b) < 0) {
 		a += mod;
 	}
 }
 
 template<class T>
-inline void mulMod(T& a, const T& b, const T& mod = 1000000007) {
+inline void mul_mod(T& a, const T& b, const T& mod = 1000000007) {
 	a = a * b % mod;
 }
 
@@ -104,5 +115,11 @@ static uint32_t __inline __clz(uint32_t x) {
 	unsigned long r = 0;
 	_BitScanReverse(&r, x);
 	return (31 - r);
+}
+
+static uint32_t __inline __ctz(uint32_t x) {
+	unsigned long r = 0;
+	_BitScanForward(&r, x);
+	return r;
 }
 #endif
