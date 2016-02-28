f8038c9725bd1ddc2442ea086f1fea2a257910a9
Alexander Agulenko <alexander.agulenko@gmail.com>
2016-02-28 18:33:44 +0300

    SegmentTree minor codestyle fix.

--- cpplib/SegmentTree.h
+++ cpplib/SegmentTree.h
@@ -11,12 +11,10 @@ public:
 
 	}; */
 
-	std::vector<T> data;
-
 	SegmentTree(const size_t N, const std::function<const T(const T&, const T&)>& pred, const T& neutral_ = static_cast<T>(0)) : N_(N), pred_(pred), neutral_(neutral_) {
 		offset_ = 1 << (31 - clz(N) + ((N & (N - 1)) == 0 ? 0 : 1));
 		size_ = offset_ << 1;
-		data.resize(size_);
+		data_.resize(size_);
 		init();
 	}
 
@@ -30,26 +28,34 @@ public:
 
 	~SegmentTree() = default;
 
-	size_t offset() const {
+	constexpr size_t offset() const {
 		return offset_;
 	}
 
-	size_t size() const {
+	constexpr size_t size() const {
 		return size_;
 	}
 
-	size_t elements_count() const {
+	constexpr size_t elements_count() const {
 		return N_;
 	}
 
+	constexpr T neutral() const {
+		return neutral_;
+	}
+
+	std::vector<T>& data() const {
+		return data_;
+	}
+
 	void init() {
-		std::fill_n(data.begin(), data.size(), neutral_);
+		std::fill_n(data_.begin(), data_.size(), neutral_);
 	}
 
 	void build(T a[]) {
-		std::copy(a, a + N_, data.begin() + offset_);
+		std::copy(a, a + N_, data_.begin() + offset_);
 		for (size_t i = offset_ - 1; i >= 1; --i) {
-			data[i] = pred_(data[i << 1], data[(i << 1) ^ 1]);
+			data_[i] = pred_(data_[i << 1], data_[(i << 1) ^ 1]);
 		}
 	}
 
@@ -57,10 +63,10 @@ public:
 		T res = neutral_;
 		for (size_t l = left + offset_, r = right + offset_; l <= r; l >>= 1, r >>= 1) {
 			if (l & 1) {
-				res = pred_(data[l++], res);
+				res = pred_(data_[l++], res);
 			}
 			if ((r & 1) == 0) {
-				res = pred_(data[r--], res);
+				res = pred_(data_[r--], res);
 			}
 		}
 		return res;
@@ -68,13 +74,14 @@ public:
 
 	void update(const size_t position, const T& value) {
 		size_t pos = position + offset_;
-		data[pos] = value;
+		data_[pos] = value;
 		for (pos >>= 1; pos >= 1; pos >>= 1) {
-			data[pos] = pred_(data[pos << 1], data[(pos << 1) ^ 1]);
+			data_[pos] = pred_(data_[pos << 1], data_[(pos << 1) ^ 1]);
 		}
 	}
 
 private:
+	std::vector<T> data_;
 	const std::function<const T(const T&, const T&)> pred_;
 	const T neutral_;
 	size_t offset_;
