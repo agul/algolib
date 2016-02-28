5f3803b693f06b830a043322754919d52574aa20
Alexander Agulenko <alexander.agulenko@gmail.com>
2016-02-29 02:33:07 +0300

    Implemented Stack class.

---
+++ cpplib/Stack.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "Head.h"
+
+template<typename T>
+class Stack {
+public:
+	using value_type = T;
+	using size_type = size_t;
+	using reference = value_type&;
+	using const_reference = const value_type&;
+	using rvalue = value_type&&;
+	using container = std::vector<value_type>;
+
+	Stack(const size_type max_size) : data_(container(max_size)), top_index_(0) {}
+	Stack() = delete;
+
+	Stack(const Stack&) = default;
+	Stack& operator =(const Stack&) = default;
+	Stack(Stack&&) = default;
+	Stack& operator =(Stack&&) = default;
+
+	reference top() {
+		return data_[top_index_ - 1];
+	}
+
+	const_reference top() const {
+		return data_[top_index_ - 1];
+	}
+
+	constexpr bool empty() const {
+		return top_index_ == 0;
+	}
+
+	constexpr size_type size() const {
+		return top_index_;
+	}
+
+	void push(const_reference value) {
+		data_[top_index_++] = value;
+	}
+
+	void push(rvalue value) {
+		data_[top_index_++] = std::move(value);
+	}
+
+	value_type pop() {
+		return data_[--top_index_];
+	}
+
+	void swap(Stack& rhs) {
+		data_.swap(rhs.data_);
+		std::swap(top_index_, rhs.top_index_);
+	}
+
+private:
+	container data_;
+	size_type top_index_;
+
+};
