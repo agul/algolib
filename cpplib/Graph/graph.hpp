2abcfa09290baaf5a2d2b10721d0c631e4556cd5
Alexander Agulenko <alexander.agulenko@gmail.com>
2016-06-11 14:55:18 +0300

    Refactored class Graph, added std::enable_if for add_edge methods.

--- cpplib/Graph/graph.hpp
+++ cpplib/Graph/graph.hpp
@@ -2,352 +2,126 @@
 #include "Head.h"
 #include "maths.hpp"
 
+enum GraphType {
+	Weighted = 1,
+	Flow = 2
+};
+
+template<typename T = ll, size_t MASK = 0>
 class Graph {
 public:
-	enum Type {
-		GT_WEIGHTED = 1,
-		GT_FLOW = 2
-	};
 
-	int *from, *to, *parent;
-	ll *weight;
-	vvi edges;
-	int maxVertexNum, maxEdgesNum;
-	int vertexCount, edgesCount;
-
-	Graph(const int maxVertexNum, const int maxEdgesNum, const int mask = 0) : maxVertexNum(maxVertexNum), maxEdgesNum(maxEdgesNum) {
-		from = new int[maxEdgesNum];
-		to = new int[maxEdgesNum];
-		edges.resize(maxVertexNum);
-		weight = (mask & GT_WEIGHTED ? new ll[maxEdgesNum] : NULL);
-		vertexCount = 0;
-		edgesCount = 0;
-	}
+	Graph() = default;
 
 	virtual ~Graph() {
-		deepClear();
+		clear();
 	}
 
-	void initGraph(const int _vertexCount) {
-		vertexCount = _vertexCount;
+	void init(const size_t vertex_count) {
+		clear();
+		vertex_count_ = vertex_count;
+		edges_.resize(vertex_count_);
 	}
 
-	bool isSparse() const {
-		return !vertexCount || sqr(static_cast<ll>(vertexCount)) >= (edgesCount << 4);
+	void clear();
+
+	template<const size_t Mask = MASK, typename std::enable_if<(Mask & GraphType::Weighted) == 0>::type* = nullptr>
+	void add_directed_edge(const size_t from, const size_t to) {
+		push_edge(from, to);
 	}
 
-	void clear(int size = -1) {
-		edgesCount = 0;
-		size = (size == -1 ? sz(edges) : std::min(size, sz(edges)));
-		if (size == -1) {
-			size = sz(edges);
-		}
-		for (int i = 0; i < size; ++i) {
-			edges[i].clear();
-		}
+	template<const size_t Mask = MASK, typename std::enable_if<(Mask & GraphType::Weighted) != 0>::type* = nullptr>
+	void add_directed_edge(const size_t from, const size_t to, const T weight) {
+		weight_.emplace_back(weight);
+		push_edge(from, to);
 	}
 
-	void deepClear() {
-		maxVertexNum = 0;
-		maxEdgesNum = 0;
-		edgesCount = 0;
-		edges.clear();
-		if (from != NULL) {
-			delete[] from;
-		}
-		if (to != NULL) {
-			delete[] to;
-		}
-		if (weight != NULL) {
-			delete[] weight;
-		}
-		if (parent != NULL) {
-			delete[] parent;
-		}
+	bool is_sparse() const {
+		return vertex_count_ == 0 || sqr(static_cast<ll>(vertex_count_)) >= (edges_count_ << 4);
 	}
 
-	int addDirectedEdge(const int _from, const int _to) {
-		from[edgesCount] = _from;
-		to[edgesCount] = _to;
-		edges[_from].emplace_back(edgesCount);
-		return edgesCount++;
+	size_t find_vertex_with_max_degree() const;
+
+protected:
+
+	void push_edge(const size_t from, const size_t to) {
+		const size_t edge_id = from_.size();
+		from_.emplace_back(from);
+		to_.emplace_back(to);
+		edges_[from].emplace_back(edge_id);
 	}
 
-	int addDirectedWeightedEdge(const int _from, const int _to, const ll _weight) {
-		weight[edgesCount] = _weight;
-		return addDirectedEdge(_from, _to);
-	}
+public:
+	std::vector<std::vector<size_t>> edges_;
+	std::vector<size_t> from_;
+	std::vector<size_t> to_;
+	std::vector<T> weight_;
 
-	bool isBipartite(int w[], int cnt[], int v, int col) const {
-		int * vertices = new int[vertexCount];
-		vertices[0] = v;
-		w[v] = col;
-		int head = 0, tail = 1;
-		while (head < tail) {
-			int curVertex = vertices[head++], curColor = w[curVertex];
-			if (cnt != NULL) {
-				++cnt[curColor];
-			}
-			for (auto& it : edges[curVertex]) {
-				int toVertex = to[it];
-				if (w[toVertex] == -1) {
-					w[toVertex] = curColor ^ 1;
-					vertices[tail++] = toVertex;
-				}
-				else
-					if (w[toVertex] != (curColor ^ 1)) {
-						delete[] vertices;
-						return false;
-					}
-			}
-		}
-		delete[] vertices;
-		return true;
-	}
-
-	bool isBipartite(int w[], int cnt[] = NULL) const {
-		std::fill_n(w, vertexCount, -1);
-		if (cnt != NULL) {
-			cnt[0] = cnt[1] = 0;
-		}
-		bool ret = true;
-		for (int i = 0; i < vertexCount && ret; ++i) {
-			if (w[i] == -1) {
-				ret &= isBipartite(w, cnt, i, 0);
-			}
-		}
-		return ret;
-	}
-
-	bool topSortAcyclic(int order[]) const
-		// non-recursive topological sorting, works only for acyclic graphs 
-	{
-		int * degree = new int[vertexCount];
-		std::fill_n(degree, vertexCount, 0);
-		for (int i = 0; i < edgesCount; ++i) {
-			++degree[to[i]];
-		}
-		int head = 0, tail = 0;
-		for (int i = 0; i < vertexCount; ++i) {
-			if (!degree[i]) {
-				order[tail++] = i;
-			}
-		}
-		while (head < tail) {
-			int curVertex = order[head++];
-			for (auto& it : edges[curVertex]) {
-				int toVertex = to[it];
-				if (!(--degree[toVertex])) {
-					order[tail++] = toVertex;
-				}
-			}
-		}
-		delete[] degree;
-		return tail == vertexCount;
-	}
-
-	void topSortRec(int order[]) const {
-		bool * used = new bool[vertexCount];
-		std::fill_n(used, vertexCount, false);
-		int cnt = 0;
-		for (int i = 0; i < vertexCount; ++i) {
-			if (!used[i]) {
-				_topSortDfs(i, order, used, cnt);
-			}
-		}
-		std::reverse(order, order + vertexCount);
-	}
-
-	void dijkstra(int startVertex, ll dist[], int last[] = NULL) const;
-	ll dijkstra(int startVertex, int finishVertex, int path[] = NULL) const;
-
-	size_t find_vertex_with_max_degree() const {
-		size_t result = 0;
-		size_t best = 0;
-		for (int i = 0; i < vertexCount; ++i) {
-			if (umax(best, edges[i].size())) {
-				result = i;
-			}
-		}
-		return result;
-	}
-
-	template<size_t N> void floyd(ll(&dist)[N][N]) const {
-		for (int i = 0; i < vertexCount; ++i) {
-			std::fill_n(dist[i], vertexCount, LINF);
-			dist[i][i] = 0;
-		}
-		for (int i = 0; i < edgesCount; ++i) {
-			ll& cur = dist[from[i]][to[i]];
-			cur = min(cur, weight[i]);
-		}
-		for (int k = 0; k < vertexCount; ++k) {
-			for (int i = 0; i < vertexCount; ++i) {
-				for (int j = 0; j < vertexCount; ++j) {
-					dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-				}
-			}
-		}
-	}
-
-private:
-	Graph();
-
-	void _topSortDfs(const int v, int order[], bool used[], int& cnt) const
-		// internal function for recursive topological sorting
-	{
-		used[v] = true;
-		for (auto& it : edges[v]) {
-			int toVertex = to[it];
-			if (!used[toVertex]) {
-				_topSortDfs(toVertex, order, used, cnt);
-			}
-		}
-		order[cnt++] = v;
-	}
+	size_t vertex_count_;
 
 };
 
-class UndirectedGraph : public Graph {
+template<typename T = ll, size_t MASK = 0>
+class UndirectedGraph : public Graph<T, MASK> {
 public:
 
-	UndirectedGraph(const int maxVertexNum, const int maxEdgesNum, const int mask = 0) : Graph(maxVertexNum, maxEdgesNum << 1, mask) {}
+	UndirectedGraph() = default;
 
-	void initGraph(const int _vertexCount) {
-		vertexCount = _vertexCount;
+	template<const size_t Mask = MASK, typename std::enable_if<(Mask & GraphType::Weighted) == 0>::type* = nullptr>
+	void add_bidirectional_edge(const size_t from, const size_t to) {
+		this->add_directed_edge(from, to);
+		this->add_directed_edge(to, from);
 	}
 
-	void addBidirectionalEdge(const int _from, const int _to) {
-		addDirectedEdge(_from, _to);
-		addDirectedEdge(_to, _from);
+	template<const size_t Mask = MASK, typename std::enable_if<(Mask & GraphType::Weighted) != 0>::type* = nullptr>
+	void add_bidirectional_edge(const size_t from, const size_t to, const T weight) {
+		this->add_directed_edge(from, to, weight);
+		this->add_directed_edge(to, from, weight);
 	}
-
-	void addBidirectionalWeightedEdge(const int _from, const int _to, const ll _weight) {
-		addDirectedWeightedEdge(_from, _to, _weight);
-		addDirectedWeightedEdge(_to, _from, _weight);
-	}
-
-private:
-	UndirectedGraph();
 
 };
 
-void Graph::dijkstra(int startVertex, ll dist[], int last[]) const {
-	std::fill_n(dist, vertexCount, LINF);
-	dist[startVertex] = 0;
-	if (last != NULL) {
-		std::fill_n(last, vertexCount, -1);
-	}
-	if (isSparse()) {
-		std::priority_queue<std::pair<ll, int>> q;
-		q.push(mp(0, startVertex));
-		while (!q.empty()) {
-			auto cur_pair = q.top();
-			q.pop();
-			int curVertex = cur_pair.Y;
-			ll curlen = -cur_pair.X;
-			if (curlen > dist[curVertex]) {
-				continue;
-			}
-			for (auto& it : edges[curVertex]) {
-				ll newlen = curlen + weight[it];
-				int toVertex = to[it];
-				if (dist[toVertex] > newlen) {
-					dist[toVertex] = newlen;
-					q.push(mp(-newlen, toVertex));
-					last[toVertex] = it;
-				}
-			}
-		}
-	}
-	else {
-		bool * used = new bool[vertexCount];
-		std::fill_n(used, vertexCount, false);
-		for (int i = 0; i < vertexCount; ++i) {
-			int index = -1;
-			ll curlen = LINF;
-			for (int j = 0; j < vertexCount; ++j) {
-				if (!used[j] && dist[j] < curlen) {
-					curlen = dist[j];
-					index = j;
-				}
-			}
-			if (index == -1) {
-				break;
-			}
-			used[index] = true;
-			for (auto& it : edges[index]) {
-				int toVertex = to[it];
-				ll newlen = curlen + weight[it];
-				if (!used[toVertex] && dist[toVertex] > newlen) {
-					dist[toVertex] = newlen;
-					last[toVertex] = it;
-				}
-			}
-		}
-		delete[] used;
-	}
+template<typename T, size_t MASK>
+void Graph<T, MASK>::clear() {
+	vertex_count_ = 0;
+	edges_.clear();
+	from_.clear();
+	to_.clear();
+	weight_.clear();
 }
 
-ll Graph::dijkstra(int startVertex, int finishVertex, int path[]) const {
-	if (startVertex == finishVertex) {
-		return 0;
-	}
-	ll * dist = new ll[vertexCount];
-	int * last = NULL;
-	if (path != NULL) {
-		last = new int[vertexCount];
-	}
-	dijkstra(startVertex, dist, last);
-	if (path != NULL) {
-		std::fill_n(path, vertexCount, -1);
-		int pathSize = 0, curVertex = finishVertex;
-		while (curVertex != startVertex) {
-			int curEdge = last[curVertex];
-			path[pathSize++] = curEdge;
-			curVertex = from[curEdge];
-		}
-		std::reverse(path, path + pathSize);
-		delete[] last;
-	}
-	ll ans = dist[finishVertex];
-	delete[] dist;
-	return ans;
+template<typename T, size_t MASK>
+size_t Graph<T, MASK>::find_vertex_with_max_degree() const {
+	const auto iter = std::max_element(edges_.begin(), edges_.end(), [](const std::vector<size_t>& lhs, const std::vector<size_t>& rhs) {
+		return lhs.size() < rhs.size();
+	});
+	return static_cast<size_t>(std::distance(edges_.begin(), iter));
 }
 
-int stronglyConnectedComponents(const Graph& g, const Graph& gr, int color[])
-// g - graph, gr - reversed graph, color - output (index of SCC for each vertex)
+/* template<typenamå T, size_t MASK>
+bool Graph::top_sort_acyclic(std::vector<size_t>& order) const
+// non-recursive topological sorting, works only for acyclic graphs 
 {
-	int n = g.vertexCount;
-	int * order = new int[n];
-	bool * used = new bool[n];
-	int * q = new int[n];
-	std::fill_n(order, n, 0);
-	g.topSortRec(order);
-	int cnt = 0;
-	std::fill_n(used, n, false);
-	for (int i = 0; i < n; ++i) {
-		int v = order[i];
-		if (!used[v]) {
-			int head = 0, tail = 1;
-			q[0] = v;
-			while (head < tail) {
-				int curVertex = q[head++];
-				color[curVertex] = cnt;
-				used[curVertex] = true;
-				for (auto& it : gr.edges[curVertex]) {
-					int to = gr.to[it];
-					if (!used[to]) {
-						used[to] = true;
-						q[tail++] = to;
-					}
-				}
-			}
-			++cnt;
+	order.clear();
+	std::vector<size_t> inbound_degree(vertex_count_, 0);
+	for (const auto& to_vertex : to_) {
+		++inbound_degree[to_vertex];
+	}
+	for (size_t i = 0; i < vertex_count_; ++i) {
+		if (inbound_degree[i] == 0) {
+			order.emplace_back(i);
 		}
 	}
-	delete[] q;
-	delete[] used;
-	delete[] order;
-	return cnt;
-}
+	size_t head = 0;
+	while (head < order.size()) {
+		const size_t cur_vertex = order[head++];
+		for (const auto& it : edges[cur_vertex]) {
+			const size_t to_vertex = to_[it];
+			--inbound_degree[to_vertex];
+			if (inbound_degree[to_vertex] == 0) {
+				order.emplace_back(to_vertex);
+			}
+		}
+	}
+	return order.size() == vertex_count_;
+} */
