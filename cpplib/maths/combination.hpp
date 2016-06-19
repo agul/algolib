d5fae9c30a7d2190efd00563d64f0c01145b98e5
Alexander Agulenko <alexander.agulenko@gmail.com>
2016-06-20 00:28:19 +0300

    Implemented Permutation and Combination classes.

---
+++ cpplib/maths/combination.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <numeric>
+#include <vector>
+
+template<typename T>
+class Combination : public std::vector<T> {
+public:
+	Combination(const size_t n, const size_t k) : std::vector<T>(k), n_(n) {
+		init();
+	}
+
+	void init() {
+		std::iota(this->begin(), this->end(), 0);
+	}
+
+	bool next() {
+		const size_t k = this->size();
+		Combination tmp = *this;
+		size_t index = k;
+		do {
+			--index;
+			if (tmp[index] < n_ - k + index) {
+				++tmp[index];
+				for (size_t j = index + 1; j < k; ++j) {
+					tmp[j] = tmp[j - 1] + 1;
+				}
+				this->swap(tmp);
+				return true;
+			}
+		} while (index > 0);
+		init();
+		return false;
+	}
+
+private:
+	size_t n_;
+
+};
---
+++ cpplib/maths/permutation.hpp
@@ -0,0 +1,60 @@
+#pragma once
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+template<typename T>
+class Permutation : public std::vector<T> {
+public:
+	Permutation() : Permutation(0) {}
+
+	explicit Permutation(const size_t size) : std::vector<T>(size) {
+		init();
+	}
+
+	template<typename I>
+	Permutation(I first, I last) : std::vector<T>(first, last) {}
+
+	Permutation(std::initializer_list<T> list) : std::vector<T>(list) {}
+
+	void init() {
+		std::iota(this->begin(), this->end(), 0);
+	}
+
+	Permutation multiply(const Permutation& rhs) const {
+		Permutation res(this->size());
+		for (size_t i = 0; i < this->size(); ++i) {
+			res[i] = this->at(rhs[i]);
+		}
+		return res;
+	}
+
+	Permutation operator *(const Permutation& rhs) const {
+		return multiply(rhs);
+	}
+
+	Permutation& operator *=(const Permutation& rhs) {
+		Permutation res = multiply(rhs);
+		this->swap(res);
+		return *this;
+	}
+
+	Permutation pow(const ll deg) const {
+		Permutation result(this->size());
+		Permutation a = *this;
+		ll b = deg;
+		while (b > 0) {
+			if ((b & 1) > 0) {
+				result *= a;
+			}
+			a *= a;
+			b >>= 1;
+		}
+		return result;
+	}
+
+	bool next() {
+		return std::next_permutation(this->begin(), this->end());
+	}
+
+};
