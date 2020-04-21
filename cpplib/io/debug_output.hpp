e935bc72486cbccca29ec4c2125218548fa43098
Alexander Agulenko <agul@yandex-team.ru>
2020-04-21 20:52:07 +0300

    Implemented debug output methods.

--- cpplib/io/debug_output.hpp
+++ cpplib/io/debug_output.hpp
@@ -2,7 +2,57 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <type_traits>
+#include <utility>
 #include <vector>
+
+#include "base/type_traits/disjunction.hpp"
+#include "base/type_traits/is_container.hpp"
+#include "base/type_traits/is_sane_integer.hpp"
+
+std::string to_string(const std::string& s) {
+	return '"' + s + '"';
+}
+
+std::string to_string(const char* s) {
+	return to_string(std::string(s));
+}
+
+std::string to_string(const char ch) {
+	return {'\'', ch, '\''};
+}
+
+std::string to_string(const bool b) {
+	return (b ? "true" : "false");
+}
+
+template<typename T, typename std::enable_if<Disjunction<is_sane_integer<T>::value, std::is_floating_point<T>::value>::value>::type* = nullptr>
+std::string to_string(const T value) {
+	return std::to_string(value);
+}
+
+template<typename A, typename B>
+std::string to_string(const std::pair<A, B>&);
+
+template<typename T, typename std::enable_if<is_container<T>::value>::type* = nullptr>
+std::string to_string(const T& container) {
+	bool is_first = true;
+	std::string res = "{";
+	for (const auto & x : container) {
+		if (!is_first) {
+			res += ", ";
+		}
+		is_first = false;
+		res += to_string(x);
+	}
+	res += "}";
+	return res;
+}
+
+template<typename A, typename B>
+std::string to_string(const std::pair<A, B>& p) {
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
 
 std::vector<std::string> vec_splitter(std::string s) {
 	s += ',';
@@ -22,10 +72,10 @@ void debug_out(
 	std::cerr << std::endl;
 }
 
-template <typename Head, typename... Tail>
+template<typename Head, typename... Tail>
 void debug_out(std::vector<std::string> args, int idx, int LINE_NUM, Head H, Tail... T) {
 	std::stringstream ss;
-	ss << H;
+	ss << to_string(H);
 	if (idx > 0) {
 		std::cerr << ", ";
 	} else {
