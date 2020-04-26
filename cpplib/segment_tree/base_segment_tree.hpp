6735b729b184e43d2514fd4a1d8d10ea316111ed
Alexander Agulenko <agul@yandex-team.ru>
2020-04-27 02:51:52 +0300

    Implemented TopDownSegmentTree and BaseSegmentTree.

---
+++ cpplib/segment_tree/base_segment_tree.hpp
@@ -0,0 +1,76 @@
+#pragma once
+#include <vector>
+
+template<typename T, typename Merge>
+class BaseSegmentTree {
+public:
+	BaseSegmentTree(
+			const size_t N,
+			const T& default_value = T(0),
+			const Merge& merge = Merge()
+	) :
+			N_(N),
+			offset_(calculate_offset(N)),
+			size_(offset_ << 1),
+			data_(size_, default_value),
+			merge_(merge),
+			default_value_(default_value)
+	{
+		init();
+	}
+
+	static constexpr size_t calculate_offset(const size_t N) {
+		return 1 << (31 - clz(N) + ((N & (N - 1)) == 0 ? 0 : 1));
+	}
+
+	constexpr size_t offset() const {
+		return offset_;
+	}
+
+	constexpr size_t size() const {
+		return size_;
+	}
+
+	constexpr size_t elements_count() const {
+		return N_;
+	}
+
+	constexpr T default_value() const {
+		return default_value_;
+	}
+
+	std::vector<T>& data() const {
+		return data_;
+	}
+
+	virtual void init() {
+		std::fill_n(data_.begin(), data_.size(), default_value_);
+	}
+
+	virtual void build(const T* a) {
+		std::copy(a, a + N_, data_.begin() + offset_);
+		for (size_t i = offset_ - 1; i >= 1; --i) {
+			merge_children(i);
+		}
+	}
+
+	virtual void build(const std::vector<T>& a) {
+		build(a.data());
+	}
+
+	void merge_children(const size_t v) {
+		data_[v] = merge_(data_[v << 1], data_[(v << 1) ^ 1]);
+	}
+
+protected:
+	size_t N_;
+	size_t offset_;
+	size_t size_;
+
+	std::vector<T> data_;
+
+	const Merge merge_;
+
+	const T default_value_;
+	
+};
---
+++ cpplib/segment_tree/top_down_segment_tree.hpp
@@ -0,0 +1,120 @@
+#pragma once
+#include <vector>
+
+#include "segment_tree/base_segment_tree.hpp"
+
+template<typename T, typename Merge, typename Update, typename ApplyUpdate, typename MergeUpdates>
+class TopDownSegmentTree : public BaseSegmentTree<T, Merge> {
+public:
+	using Base = BaseSegmentTree<T, Merge>;
+
+	TopDownSegmentTree(
+			const size_t N,
+			const T& default_value = T(0),
+			const Merge& merge = Merge(),
+			const Update& default_update = Update(),
+			const ApplyUpdate& apply_update = ApplyUpdate(),
+			const MergeUpdates& merge_updates = MergeUpdates()
+	) :
+			Base(N, default_value, merge),
+			updates_(size_, default_update),
+			default_update_(default_update),
+			apply_update_(apply_update),
+			merge_updates_(merge_updates)
+	{
+		init();
+	}
+
+	void init() {
+		std::fill_n(updates_.begin(), updates_.size(), 0);
+	}
+
+	void point_update(const size_t index, const Update& update) {
+		range_update(index, index, update);
+	}
+
+	void range_update(const size_t left, const size_t right, const Update& update) {
+		update_impl(1, 0, offset_ - 1, left, right, update);
+	}
+
+	T get(const size_t index) {
+		return get(index, index);
+	}
+
+	T get(const size_t left, const size_t right) {
+		return get_impl(1, 0, offset_ - 1, left, right);
+	}
+
+protected:
+	using Base::offset_;
+	using Base::size_;
+	using Base::data_;
+	using Base::merge_;
+	using Base::default_value_;
+
+private:
+	std::vector<Update> updates_;
+	const Update default_update_;
+	const ApplyUpdate apply_update_;
+	const MergeUpdates merge_updates_;
+
+	static constexpr size_t get_mid(const size_t cur_left, const size_t cur_right) {
+		return (cur_left + cur_right) / 2;
+	}
+
+	T get_impl(const size_t v, const size_t cur_left, const size_t cur_right, const size_t left, const size_t right) {
+		if (right < cur_left || left > cur_right) {
+			return default_value_;
+		}
+		if (left <= cur_left && cur_right <= right) {
+			return data_[v];
+		}
+		push_impl(v, cur_left, cur_right);
+		const size_t mid = get_mid(cur_left, cur_right);
+		return merge_(
+				get_impl(v << 1, cur_left, mid, left, right),
+				get_impl((v << 1) ^ 1, mid + 1, cur_right, left, right)
+		);
+	}
+
+	void update_impl(const size_t v, const size_t cur_left, const size_t cur_right, const size_t left, const size_t right, const Update& update) {
+		if (right < cur_left || left > cur_right) {
+			return;
+		}
+		if (left <= cur_left && cur_right <= right) {
+			data_[v] = apply_update_(data_[v], update, cur_left, cur_right);
+			updates_[v] = merge_updates_(updates_[v], update);
+			return;
+		}
+		push_impl(v, cur_left, cur_right);
+
+		const size_t mid = get_mid(cur_left, cur_right);
+		update_impl(v << 1, cur_left, mid, left, right, update);
+		update_impl((v << 1) ^ 1, mid + 1, cur_right, left, right, update);
+
+		this->merge_children(v);
+	}
+
+	void push_impl(const size_t v, const size_t cur_left, const size_t cur_right) {
+		const size_t mid = get_mid(cur_left, cur_right);
+
+		data_[v << 1] = apply_update_(data_[v << 1], updates_[v], cur_left, mid);
+		updates_[v << 1] = merge_updates_(updates_[v << 1], updates_[v]);
+
+		data_[(v << 1) ^ 1] = apply_update_(data_[(v << 1) ^ 1], updates_[v], mid + 1, cur_right);
+		updates_[(v << 1) ^ 1] = merge_updates_(updates_[(v << 1) ^ 1], updates_[v]);
+
+		updates_[v] = default_update_;
+	}
+
+};
+
+template<typename T, typename Update = T>
+struct Applier {
+	virtual T operator()(const T& value, const Update& update, const size_t left, const size_t right) const = 0;
+};
+
+template<typename Update>
+struct Merger {
+	virtual Update operator()(const Update& acc_updates, const Update& update) const = 0;
+};
