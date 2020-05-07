e7d8344b804e8381407b3a9a5805707f26336981
Alexander Agulenko <agul@yandex-team.ru>
2020-05-08 01:53:36 +0300

    Remove odd include.

--- cpplib/base/header.hpp
+++ cpplib/base/header.hpp
@@ -35,6 +35,5 @@
 #include "base/safe_integral_hash.hpp"
 #include "base/typedefs.hpp"
 #include "io/debug_output.hpp"
-#include "base/typedefs.hpp"
 #include "io/io.hpp"
 #include "range/ranges.hpp"
