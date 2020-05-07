d0fc0a7825d63b92f5ebe3d5ec498c8898b8b31e
Alexander Agulenko <agul@yandex-team.ru>
2020-05-07 15:52:21 +0300

    Refactored random.hpp: make it more stable.

--- cpplib/maths/random.hpp
+++ cpplib/maths/random.hpp
@@ -5,25 +5,43 @@
 
 namespace Random {
 
-static std::chrono::system_clock::rep current_timestamp() {
+inline std::chrono::system_clock::rep current_timestamp() {
 	return std::chrono::system_clock::now().time_since_epoch().count();
 }
 
-static uint64_t get_rand_seed() {
+inline uint64_t get_new_rand_seed() {
 	return static_cast<uint64_t>(current_timestamp());
 }
 
-static std::mt19937_64 engine(get_rand_seed());
-static std::uniform_int_distribution<uint64_t> distribution(0, std::numeric_limits<uint64_t>::max());
+inline uint64_t get_rand_seed() {
+	static const uint64_t kRandSeed = get_new_rand_seed();
+	return kRandSeed;
+}
+
+using Engine = std::mt19937_64;
 
 template<typename T>
-static T get(const T right) {
-	return distribution(engine) % right;
+using Distribution = std::uniform_int_distribution<T>;
+
+inline Engine& random_engine() {
+	static Engine engine(get_rand_seed());
+	return engine;
 }
 
 template<typename T>
-static T get(const T left, const T right) {
-	return get(right - left + 1) + left;
+inline T get(const T to) {
+	Distribution<T> distribution(0, to);
+	return distribution(random_engine());
+}
+
+template<typename T>
+inline T get(const T from, const T to) {
+	return get(to - from) + from;
+}
+
+template<typename T = uint64_t>
+inline T get() {
+	return get(static_cast<T>(0), std::numeric_limits<T>::max());
 }
 
 }  // namespace Random
