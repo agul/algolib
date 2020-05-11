e9d69288b7edd67fb867be8a3a24c633bf9bde5e
Alexander Agulenko <agul@yandex-team.ru>
2020-05-11 20:18:16 +0300

    Refactored Hasher and DoubleHasher classes (merged agul/hasher branch).

---
+++ cpplib/hash/README.md
@@ -0,0 +1,3 @@
+### `Hasher` (including `DoubleHasher`)
+Used and tested at:
+- 2020-05-11 = **[Codeforces 611D](https://codeforces.com/contest/611/problem/D)** (https://github.com/agul/contest-tasks/blob/master/DNoviiGodIDrevneeProrochestvo.cpp)
--- cpplib/hash/hasher.hpp
+++ cpplib/hash/hasher.hpp
@@ -1,160 +1,65 @@
 #pragma once
+#include <algorithm>
+#include <cstddef>
 #include <cstdlib>
+#include <vector>
 
 #include "maths/maths.hpp"
+#include "maths/random.hpp"
 #include "string/string_view.hpp"
+#include "double_hasher.hpp"
 
-class SingleHasher {
-public:
-
-	static const long long BASE = 277;
-	static const int MOD = 1000000007;
-
-	long long * hash, *deg;
-
-	explicit SingleHasher(const int n) {
-		hash = new long long[n + 1];
-		deg = new long long[n + 1];
-		calc_powers(deg, BASE, n);
-	}
-
-	~SingleHasher() {
-		delete[] hash;
-		delete[] deg;
-	}
-
-	void hashString(const char * s, int n = 0)
-		// hash begins at index 1
-	{
-		if (!n) {
-			n = strlen(s);
-		}
-		hash[0] = 0;
-		for (int i = 0; i < n; ++i) {
-			hash[i + 1] = (hash[i] * BASE + s[i]) % MOD;
-		}
-	}
-
-	void hashString(const std::string& s) {
-		hashString(s.c_str(), s.length());
-	}
-
-	long long getHash(const int l, const int r) const {
-		long long result = (hash[r] - hash[l - 1] * deg[r - l + 1]) % MOD;
-		if (result < 0) {
-			result += MOD;
-		}
-		return result;
-	}
-
-};
-
-class DoubleHasher_t {
-public:
-	static const uint32_t MA = static_cast<uint32_t>(1e9) + 7;
-	static const uint32_t MB = static_cast<uint32_t>(1e9) + 9;
-
-	int a, b;
-
-	DoubleHasher_t() {}
-	DoubleHasher_t(const int x) : a(x), b(x) {}
-	DoubleHasher_t(const int a, const int b) : a(a), b(b) {}
-
-	DoubleHasher_t operator + (const DoubleHasher_t &x) const {
-		return DoubleHasher_t((a + x.a) % MA, (b + x.b) % MB);
-	}
-
-	DoubleHasher_t operator - (const DoubleHasher_t &x) const {
-		return DoubleHasher_t((a + MA - x.a) % MA, (b + MB - x.b) % MB);
-	}
-
-	DoubleHasher_t operator * (const int x) const {
-		return DoubleHasher_t(((long long)a * x) % MA, ((long long)b * x) % MB);
-	}
-
-	DoubleHasher_t operator * (const DoubleHasher_t &x) const {
-		return DoubleHasher_t(((long long)a * x.a) % MA, ((long long)b * x.b) % MB);
-	}
-
-	bool operator == (const DoubleHasher_t &x) const {
-		return a == x.a && b == x.b;
-	}
-
-	bool operator < (const DoubleHasher_t& x) {
-		return a < x.a || (a == x.a && b < x.b);
-	}
-
-	explicit operator long long() const {
-		return (long long)a * MB + b + 1;
-	}
-
-};
-
-template<class T>
+template<typename T, typename Container = StringView>
 class Hasher {
 public:
+	using size_type = std::size_t;
+	using container_type = Container;
+	using value_type = T;
 
-	T * deg, * hash;
-	int P;
-	StringView str;
+	explicit Hasher(const container_type& data) : size_(data.size()), hash_(size_ + 1), data_(data) {
+		const value_type kHashPoint = Random::get(307, 877117);
+		deg_ = calc_powers(kHashPoint, size_ + 1);
 
-	explicit Hasher(const int n, const int P = std::max(rand(), 307)) : P(P) {
-		hash = new T[n + 1];
-		deg = new T[n + 1];
-		deg[0] = 1;
-		for (int i = 1; i <= n; ++i) {
-			deg[i] = deg[i - 1] * P;
+		hash_[0] = 0;
+		for (size_type i = 0; i < data_.size(); ++i) {
+			hash_[i + 1] = hash_[i] * kHashPoint + data_[i];
 		}
 	}
 
-	~Hasher() {
-		delete[] hash;
-		delete[] deg;
+	value_type get_hash(const size_type left, const size_type right) const
+	// returns hash for range [left, right]
+	{
+		return hash_[right + 1] - hash_[left] * deg_[right - left + 1];
 	}
 
-	void hashString(const StringView& s) {
-		str = s;
-		hash[0] = 0;
-		for (int i = 0; i < s.length(); ++i) {
-			hash[i + 1] = hash[i] * P + s[i];
-		}
-	}
-
-	template<typename U>
-	void hash_vector(const std::vector<U>& vec) {
-		hash[0] = 0;
-		for (int i = 0; i < vec.size(); ++i) {
-			hash[i + 1] = hash[i] * P + vec[i];
-		}
-	}
-
-	T getHash(const int l, const int r) const {
-		return hash[r] - hash[l - 1] * deg[r - l + 1];
-	}
-
-	int lcp(const int i, const int j) const {
-		int L = 0, R = n - max(i, j);
-		while (L < R) {
-			int M = (L + R + 1) >> 1;
-			if (getHash(i, i + M - 1) == getHash(j, j + M - 1)) {
-				L = M;
+	size_type lcp(const size_type a, const size_type b) const {
+		size_type L = 0;
+		size_type R = size_ - std::max(a, b) + 1;
+		while (R - L > 1) {
+			const size_type md = (L + R + 1) / 2;
+			if (get_hash(a, a + md - 1) == get_hash(b, b + md - 1)) {
+				L = md;
 			} else {
-				R = M - 1;
+				R = md;
 			}
 		}
 		return L;
 	}
 
-	bool less(const int i, const int j) const {
-		return cmpSubstrings(i, j) < 0;
+	bool compare_substrings(const size_type a, const size_type b) const {
+		return compare_substrings_impl(a, b);
 	}
 
 private:
-	Hasher();
+	size_type size_;
+	std::vector<value_type> deg_;
+	std::vector<value_type> hash_;
+	const container_type& data_;
 
-	int cmpSubstrings(int a, int b) const {
-		int LEN = n - max(a, b), L = lcp(a, b);
-		return L < LEN ? (int)str[a + L] - str[b + L] : b - a;
+	bool compare_substrings_impl(size_type a, size_type b) const {
+		const size_type max_substring_length = size_ - std::max(a, b);
+		const size_type lcp_length = lcp(a, b);
+		return lcp_length < max_substring_length ? data_[a + lcp_length] < data_[b + lcp_length] : b < a;
 	}
 
 };
