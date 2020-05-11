6a1d42673a889773bb41c4f85012cdc46dbbedbc
Alexander Agulenko <agul@yandex-team.ru>
2020-05-11 15:18:17 +0300

    Refactored SparseTableCmp<> and added README.md for data_structures/ directory.

---
+++ cpplib/data_structures/README.md
@@ -0,0 +1,3 @@
+### `SparseTableCmp<>`
+Used and tested at:
+- 2020-05-11 = **[Codeforces 689D](https://codeforces.com/contest/689/problem/D)** (https://github.com/agul/contest-tasks/blob/master/DDruzyaIPosledovatelnosti.cpp)
--- cpplib/data_structures/sparse_table.hpp
+++ cpplib/data_structures/sparse_table.hpp
@@ -2,48 +2,48 @@
 #include <functional>
 #include <vector>
 
-template<typename T>
+template<typename T, typename Cmp = std::less<T>>
 class SparseTableCmp {
 public:
-	using Comparator = std::function<bool(const T&, const T&)>;
+	using value_type = T;
+	using size_type = std::size_t;
+	using comparator_type = Cmp;
+	using container_type = std::vector<value_type>;
 
-	SparseTableCmp(const size_t size, const Comparator& less) :
-		size_(size), less_(less), log_table_(size + 1, 0) {
-		for (size_t i = 2; i <= size_; ++i) {
+	explicit SparseTableCmp(const container_type& data, const comparator_type& cmp = comparator_type()) :
+			size_(data.size()),
+			cmp_(cmp),
+			log_table_(size_ + 1, 0),
+			data_(data)
+	{
+		for (size_type i = 2; i <= size_; ++i) {
 			log_table_[i] = log_table_[i >> 1] + 1;
 		}
-		data_.assign(log_table_[size_] + 1, std::vector<size_t>(size_));
-	}
+		table_.assign(log_table_[size_] + 1, std::vector<size_type>(size_));
 
-	void set_comparator(const Comparator& less) {
-		less_ = less;
-	}
-
-	void build(const std::vector<T>& vec) {
-		arr_ = &vec;
-		for (size_t i = 0; i < size_; ++i) {
-			data_[0][i] = i;
-		}
-		for (size_t level = 1, step = 2; step < size_; ++level, step <<= 1) {
-			for (size_t i = 0; i + step <= size_; ++i) {
-				const size_t x = data_[level - 1][i];
-				const size_t y = data_[level - 1][i + (step >> 1)];
-				data_[level][i] = (less_(arr_->at(x), arr_->at(y)) ? x : y);
+		std::iota(table_[0].begin(), table_[0].end(), 0);
+		for (size_type level = 1, step = 2; step < size_; ++level, step <<= 1) {
+			for (size_type i = 0; i + step <= size_; ++i) {
+				const size_type x = table_[level - 1][i];
+				const size_type y = table_[level - 1][i + (step >> 1)];
+				table_[level][i] = (cmp_(data_.at(x), data_.at(y)) ? x : y);
 			}
 		}
 	}
 
-	size_t query(const size_t left, const size_t right) const {
-		const size_t level = log_table_[right - left - 1];
-		const size_t x = data_[level][left];
-		const size_t y = data_[level][right - (1 << level)];
-		return (less_(arr_->at(x), arr_->at(y)) ? x : y);
+	size_type query(const size_type left, const size_type right) const
+	// query for range [left, right)
+	{
+		const size_type level = log_table_[right - left - 1];
+		const size_type x = table_[level][left];
+		const size_type y = table_[level][right - (1 << level)];
+		return (cmp_(data_.at(x), data_.at(y)) ? x : y);
 	}
 
 private:
-	std::vector<std::vector<size_t>> data_;
-	std::vector<size_t> log_table_;
-	Comparator less_;
-	const std::vector<T>* arr_;
-	size_t size_;
+	size_type size_;
+	std::vector<std::vector<size_type>> table_;
+	std::vector<size_type> log_table_;
+	const comparator_type& cmp_;
+	const container_type& data_;
 };
