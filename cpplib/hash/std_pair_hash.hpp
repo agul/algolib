d9db0074dac64cd8a6a4915d748d2c78a1dd4ca6
Alexander Agulenko <agul@yandex-team.ru>
2020-05-17 21:14:10 +0300

    Added jngen (forked from ifsmirnov/jngen).

--- cpplib/hash/std_pair_hash.hpp
+++ cpplib/hash/std_pair_hash.hpp
@@ -4,11 +4,13 @@
 
 namespace std {
 
+#ifndef STD_HASH_PAIR
 template<typename T, typename U>
 struct hash<std::pair<T, U>> {
 	std::size_t operator()(const std::pair<T, U>& pair) const {
 		return pair.first * 877117 + pair.second;
 	}
 };
+#endif
 
 }  // namespace std
