87b8caff2ec299b82fa1f8589b9717cef931141a
Alexander Agulenko <agul@yandex-team.ru>
2020-06-17 21:01:50 +0300

    Optimized FenwickTreeSum<> and added tests for it.

--- cpplib/data_structures/fenwick_tree.hpp
+++ cpplib/data_structures/fenwick_tree.hpp
@@ -10,20 +10,27 @@ public:
 	using size_type = std::size_t;
 	using container_type = std::vector<value_type>;
 
-	explicit FenwickTreeSum(const size_type n) : size_(n), data_(n, 0) {}
+	explicit FenwickTreeSum(const size_type n) : data_(n, 0) {}
 
-	explicit FenwickTreeSum(const std::vector<value_type>& a) : size_(a.size()) {
-		for (size_type i = 0; i < size_; ++i) {
-			inc(i, a[i]);
+	explicit FenwickTreeSum(const std::vector<value_type>& a) : data_(a) {
+		for (size_type i = 0; i < data_.size(); ++i) {
+			const size_type tree_index = (i | (i + 1));
+			if (tree_index < data_.size()) {
+				data_[tree_index] += data_[i];
+			}
 		}
 	}
 
+	bool operator ==(const FenwickTreeSum& rhs) const {
+		return data_ == rhs.data_;
+	}
+
 	void clear() {
-		data_.assign(size_, 0);
+		data_.assign(data_.size(), 0);
 	}
 
 	size_type size() const {
-		return size_;
+		return data_.size();
 	}
 
 	container_type& data() {
@@ -34,33 +41,43 @@ public:
 		return data_;
 	}
 
-	void inc(int v, const value_type delta) {
-		for (; v < size_; v = (v | (v + 1))) {
+	void inc(const size_type index, const value_type delta) {
+		for (size_type v = index; v < data_.size(); v = (v | (v + 1))) {
 			data_[v] += delta;
 		}
 	}
 
-	value_type query(int v) const
+	value_type query(const size_type index) const
 	// returns sum for range [0, index]
 	{
 		value_type res = 0;
-		for (; v >= 0; v = (v & (v + 1)) - 1) {
+		for (int32_t v = index; v >= 0; v = (v & (v + 1)) - 1) {
 			res += data_[v];
 		}
 		return res;
 	}
 
-	value_type query(const int left, const int right) const
+	value_type query(const size_type left, const size_type right) const
 	// returns sum for range [left, right]
 	{
 		if (right < left) {
 			return 0;
 		}
-		return query(right) - query(left - 1);
+		value_type res = 0;
+		int32_t r = right;
+		int32_t l = std::max(0, static_cast<int32_t>(left) - 1);
+		for (; r >= l; r = (r & (r + 1)) - 1) {
+			res += data_[r];
+		}
+		if (left > 0) {
+			for (; l > r; l = (l & (l + 1)) - 1) {
+				res -= data_[l];
+			}
+		}
+		return res;
 	}
 
 private:
-	size_type size_;
 	container_type data_;
 };
 
