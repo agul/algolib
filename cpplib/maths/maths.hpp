bf82277ff892da01a45a22300bb80002a86d06d8
Alexander Agulenko <alexander.agulenko@gmail.com>
2021-03-21 12:17:42 +0300

    Added quadratic equation solver.

--- cpplib/maths/maths.hpp
+++ cpplib/maths/maths.hpp
@@ -81,6 +81,22 @@ bool solve_diophantine(const T a, const T b, const T c, T& x, T& y, T& g) {
     return true;
 }
 
+template<typename T, typename std::enable_if<std::is_floating_point<T>::value>::type* = nullptr>
+size_t solve_quadratic_equation(const T a, const T b, const T c, T& x1, T& x2) {
+    const T D = sqr(b) - 4 * a * c;
+    if (D < -EPS) {
+        return 0;
+    }
+    if (is_equal_to_zero(D)) {
+        const T x = -b / (a * 2);
+        x1 = x;
+        return 1;
+    }
+    x1 = (-b + std::sqrt(D)) / (a * 2);
+    x2 = (-b - std::sqrt(D)) / (a * 2);
+    return 2;
+}
+
 template<typename T>
 T factorial(const size_t n) {
 	T ret = 1;
