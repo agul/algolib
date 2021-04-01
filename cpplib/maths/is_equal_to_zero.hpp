4b3b587930fa1a196bf56c8c1d047edee548dc79
Alexander Agulenko <alexander.agulenko@gmail.com>
2021-04-01 23:35:37 +0300

    Added #include in is_equal_to_zero.hpp to make it working with Decimal.

--- cpplib/maths/is_equal_to_zero.hpp
+++ cpplib/maths/is_equal_to_zero.hpp
@@ -3,10 +3,11 @@
 #include <type_traits>
 
 #include "base/constants.hpp"
+#include "maths/decimal.hpp"
 
 template<typename T, typename std::enable_if<std::is_floating_point<T>::value>::type* = nullptr>
 inline constexpr bool is_equal_to_zero(const T& a) {
-	return abs(a) < EPS;
+	return std::abs(a) < EPS;
 }
 
 template<typename T, typename std::enable_if<!std::is_floating_point<T>::value>::type* = nullptr>
