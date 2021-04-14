900ef266217de60008838e8f9f68b667f90201a0
Alexander Agulenko <alexander.agulenko@gmail.com>
2021-04-14 21:00:46 +0300

    Added as_container<>() method for conversion between STL containers.

--- cpplib/base/helpers.hpp
+++ cpplib/base/helpers.hpp
@@ -9,39 +9,45 @@
 #include "hash/safe_integral_hash.hpp"
 #include "hash/std_pair_hash.hpp"
 #include "constants.hpp"
+#include "type_traits/is_container.hpp"
 
 template<typename T>
 inline bool umin(T& a, const T& b) {
-	return (b < a ? a = b, true : false);
+    return (b < a ? a = b, true : false);
 }
 
 template<typename T>
 inline bool umax(T& a, const T& b) {
-	return (a < b ? a = b, true : false);
+    return (a < b ? a = b, true : false);
 }
 
 template<typename T, size_t N>
 struct MakeVector {
-	template<typename... Args,
-		typename R = std::vector<decltype(MakeVector<T, N - 1>::make_vector(std::declval<Args>()...)) >>
-		static R make_vector(size_t first, Args... sizes) {
-		auto inner = MakeVector<T, N - 1>::make_vector(sizes...);
-		return R(first, inner);
-	}
+    template<typename... Args,
+            typename R = std::vector<decltype(MakeVector<T, N - 1>::make_vector(std::declval<Args>()...)) >>
+    static R make_vector(size_t first, Args... sizes) {
+        auto inner = MakeVector<T, N - 1>::make_vector(sizes...);
+        return R(first, inner);
+    }
 };
 
 template<typename T>
 struct MakeVector<T, 1> {
-	/// caide keep
-	template<typename R = std::vector<T>>
-	static R make_vector(std::size_t size, const T& value) {
-		return R(size, value);
-	}
+    /// caide keep
+    template<typename R = std::vector<T>>
+    static R make_vector(std::size_t size, const T& value) {
+        return R(size, value);
+    }
 };
 
 template<typename T, typename... Args>
 auto make_vector(Args... args) -> decltype(MakeVector<T, sizeof...(Args)-1>::make_vector(args...)) {
-	return MakeVector<T, sizeof...(Args)-1>::make_vector(args...);
+    return MakeVector<T, sizeof...(Args)-1>::make_vector(args...);
+}
+
+template<typename T, typename U, typename std::enable_if<is_container<T>::value && is_container<U>::value>::type* = nullptr>
+T as_container(const U& collection) {
+    return T{std::begin(collection), std::end(collection)};
 }
 
 #define X first
