1f7919abc7e6963d1795001446670c256981e385
Alexander Agulenko <alexander.agulenko@gmail.com>
2021-04-29 10:01:30 +0300

    Huge refactoring in FenwickTreeRangeUpdates.

--- cpplib/data_structures/fenwick_tree/fenwick_tree_range_updates.hpp
+++ cpplib/data_structures/fenwick_tree/fenwick_tree_range_updates.hpp
@@ -1,71 +1,65 @@
 #pragma once
 #include <algorithm>
+#include <cstddef>
+#include <type_traits>
+#include <vector>
 
 template<typename T>
 class FenwickTreeSumRangeUpdates {
 public:
-    T * add, * mul;
-    int N;
+    using index_type = int32_t;
+    static_assert(std::is_signed<index_type>::value, "Index type in fenwick tree should be signed integer");
 
-    FenwickTreeSumRangeUpdates(const int N) : N(N) {
-        add = new T[N];
-        mul = new T[N];
-        std::fill_n(add, N, 0);
-        std::fill_n(mul, N, 0);
-    }
+    using size_type = std::size_t;
+    using value_type = T;
+    using container_type = std::vector<value_type>;
 
-    ~FenwickTreeSumRangeUpdates() {
-        delete[] add;
-        delete[] mul;
-    }
+    explicit FenwickTreeSumRangeUpdates(const size_type n) : add_(n, 0), mul_(n, 0) {}
 
-    void clear() {
-        std::fill_n(add, N, 0);
-        std::fill_n(mul, N, 0);
-    }
-
-    void setSize(const int n) {
-        N = n;
-    }
-
-    void build(T a[], const int n) {
-        N = n;
-        for (int i = 0; i < N; ++i) {
+    explicit FenwickTreeSumRangeUpdates(const std::vector<value_type>& a) : FenwickTreeSumRangeUpdates(a.size()) {
+        for (size_type i = 0; i < a.size(); ++i) {
             update(i, i, a[i]);
         }
     }
 
-    void update(const int left, const int right, const T by) {
-        if (left > right) {
+    void clear() {
+        add_.assign(add_.size(), 0);
+        mul_.assign(mul_.size(), 0);
+    }
+
+    void update(const index_type left, const index_type right, const value_type delta) {
+        if (right < left) {
             return;
         }
-        _update(left, by, -by * (left - 1));
-        _update(right, -by, by * right);
+        update_impl(left, delta, -delta * (left - 1));
+        update_impl(right, -delta, delta * right);
     }
 
-    T query(int at) const {
-        T resMul = 0;
-        T resAdd = 0;
-        int start = at;
+    [[nodiscard]] value_type query(index_type at) const {
+        value_type result_multiply = 0;
+        value_type result_add = 0;
+        const index_type start = at;
         while (at >= 0) {
-            resMul += mul[at];
-            resAdd += add[at];
+            result_multiply += mul_[at];
+            result_add += add_[at];
             at = (at & (at + 1)) - 1;
         }
-        return resMul * start + resAdd;
+        return result_multiply * start + result_add;
     }
 
-    T query(const int left, const int right) const {
+    [[nodiscard]] value_type query(const index_type left, const index_type right) const {
         return query(right) - query(left - 1);
     }
 
 private:
-    void _update(int at, const T updMul, const T updAdd) {
-        while (at < N) {
-            mul[at] += updMul;
-            add[at] += updAdd;
+    container_type add_;
+    container_type mul_;
+
+    void update_impl(index_type at, const value_type update_multiply, const value_type update_add) {
+        while (at < add_.size()) {
+            mul_[at] += update_multiply;
+            add_[at] += update_add;
             at |= (at + 1);
         }
     }
-
 };
